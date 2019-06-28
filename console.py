3057eecfb253c08a5563cf47a913131018631af9
Jason Cortella <419@holbertonschool.com>
2019-06-28 20:14:11 +0000

    Pushing for file syncing

--- console.py
+++ console.py
@@ -14,5 +14,26 @@ class HBNBCommand(cmd.Cmd):
         """EOF command to exit the program"""
         return True
 
+    def do_create(self, *args):
+        """create command to exit the program"""
+        return True #PLACEHOLDER
+
+    def do_show(self, *args):
+        """show command to exit the program"""
+        return True #PLACEHOLDER
+
+    def do_destroy(self, *args):
+        """destroy command to exit the program"""
+        return True #PLACEHOLDER
+
+    def do_all(self, *args):
+        """all command to exit the program"""
+        return True #PLACEHOLDER
+
+    def do_update(self, *args):
+        """update command to exit the program"""
+        return True #PLACEHOLDER
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
