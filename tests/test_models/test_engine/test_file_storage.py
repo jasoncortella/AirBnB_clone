098426a925b6f8f661b0a5f66dcf95d802448e0c
Jason Cortella <419@holbertonschool.com>
2019-07-02 05:55:32 +0000

    Add file storage unit tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -4,14 +4,38 @@
 
 import unittest
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
+from datetime import datetime
+from time import sleep
+import models
+import os
 
-class test_first_file_storage(unittest.TestCase):
-    """ define unit test for testing file storage tests """
+class test_file_storage_instantiation(unittest.TestCase):
+    """ define unit test for testing file storage instantiation tests """
 
-    def test_file_storage_no_args(self):
-        a = BaseModel()
-        self.assertGreater(len(a.id), 0)
+    def test_filestorage_instantiation(self):
+        a = FileStorage()
+        self.assertIsInstance(a, FileStorage)
+
+    def test_filestorage_instantiation_with_arg(self):
+        with self.assertRaises(TypeError):
+            a = FileStorage("arg")
+
+    def test_filestorage_file_path_private(self):
+        a = FileStorage()
+        with self.assertRaises(AttributeError):
+            a.file_path
+
+    def test_filestorage_objects_private(self):
+        a = FileStorage()
+        with self.assertRaises(AttributeError):
+            a.objects
 
 
 if __name__ == '__main__':
