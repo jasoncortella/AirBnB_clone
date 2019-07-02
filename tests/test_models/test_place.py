ce4fe2a1feaa0f74c51867b1e53802f993bff669
JosefGoodyear <josefgoodyear@gmail.com>
2019-07-02 20:39:09 +0000

    update test place file

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,10 +10,11 @@ from time import sleep
 import models
 import os
 
-class test_place_class_instantiation(unittest.TestCase):
-    """ define unittest for testing the id instance attribute """
 
-    def test_place_class_instantiation(self):
+class test_place_instantiation(unittest.TestCase):
+    """ define unittest for testing the place class """
+
+    def test_place_instantiation(self):
         a = Place()
         self.assertIsInstance(a, Place)
 
@@ -21,3 +22,54 @@ class test_place_class_instantiation(unittest.TestCase):
         a = Place()
         self.assertIsInstance(a, BaseModel)
 
+    def test_place_public_attributes(self):
+        a = Place()
+        a.city_id = "city"
+        a.user_id = "user"
+        a.name = "name"
+        a.description = "description"
+        a.number_rooms = 5
+        a.number_bathrooms = 4
+        a.max_guest = 5
+        a.price_by_night = 100
+        a.latitude = 60.5
+        a.longitude = 50.5
+        a.amenity_ids = ["this", "is", "a", "list"]
+        self.assertEqual(a.city_id, "city")
+        self.assertEqual(a.user_id, "user")
+        self.assertEqual(a.name, "name")
+        self.assertEqual(a.description, "description")
+        self.assertEqual(a.number_rooms, 5)
+        self.assertEqual(a.number_bathrooms, 4)
+        self.assertEqual(a.max_guest, 5)
+        self.assertEqual(a.price_by_night, 100)
+        self.assertEqual(a.longitude, 50.5)
+        self.assertEqual(a.latitude, 60.5)
+        self.assertEqual(a.amenity_ids, ["this", "is", "a", "list"])
+
+    def test_place_attributes_types(self):
+        a = Place()
+        self.assertEqual(type(a.city_id), str)
+        self.assertEqual(type(a.user_id), str)
+        self.assertEqual(type(a.name), str)
+        self.assertEqual(type(a.description), str)
+        self.assertEqual(type(a.number_rooms), int)
+        self.assertEqual(type(a.number_bathrooms), int)
+        self.assertEqual(type(a.max_guest), int)
+        self.assertEqual(type(a.price_by_night), int)
+        self.assertEqual(type(a.longitude), float)
+        self.assertEqual(type(a.latitude), float)
+        self.assertEqual(type(a.amenity_ids), list)
+
+    def test_place_args_unused(self):
+        a = Place("argument")
+        self.assertNotIn("argument", a.__dict__.values())
+
+    def test_place_args_unused_with_kwargs(self):
+        a = Place("argument", city_id="Oakland")
+        self.assertNotIn("argument", a.__dict__.values())
+        self.assertEqual(a.city_id, "Oakland")
+
+    def test_place_instance_is_in_objects(self):
+        a = Place()
+        self.assertIn(a, models.storage.all().values())
